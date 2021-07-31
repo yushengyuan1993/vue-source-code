33af44cb6fd5e4b5d778a47a82f46982fac2efcc
yushare <582718643@qq.com>
2021-07-31 23:34:03 +0800

    vue-router

--- vue-router/index.js
+++ vue-router/index.js
@@ -34,6 +34,7 @@ export default class VueRouter {
   init() {
     this.createRouteMap();
     this.initComponents(_Vue);
+    this.initEvent();
   }
 
   createRouteMap() {
@@ -52,10 +53,34 @@ export default class VueRouter {
         return h('a', {
           attrs: {
             href: this.to
+          },
+          on: {
+            click: this.clickHandler
+          },
+          methods: {
+            clickHandler(e) {
+              history.pushState({}, '', this.to);
+              this.$router.data.current = this.to;
+              e.preventDefault();
+            }
           }
         }, [this.$slots.default])
       },
       // template: '<a :href="to"><slot></slot></a>'
+    })
+
+    const self = this;
+    Vue.component('router-view', {
+      render(h) {
+        const component = self.routeMap[self.data.current];
+        return h(component);
+      }
+    })
+  }
+
+  initEvent() {
+    window.addEventListener('popstate', () => {
+      this.data.current = window.location.pathname;
     })
   }
 }
