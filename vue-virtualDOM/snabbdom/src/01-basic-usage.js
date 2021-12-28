1e7c93f5d0eb0d61bd0029836966af5f702ea100
yushare <582718643@qq.com>
2021-12-28 23:49:58 +0800

    feat: snabbdom modules

--- vue-virtualDOM/snabbdom/src/01-basic-usage.js
+++ vue-virtualDOM/snabbdom/src/01-basic-usage.js
@@ -4,8 +4,14 @@ import { h } from 'snabbdom/build/h'
 const patch = init([])
 
 let app = document.querySelector('#app')
+
+// 第一个参数：标签+选择器
+// 第二个参数：如果是字符串就是标签中的文本内容
 let vnode = h('div#container.v1', 'Hello World')
 
+// 第一个参数：旧的 VNode，可以是 DOM 元素
+// 第二个参数：新的 VNode
+// 返回新的 VNode
 let oldVnode = patch(app, vnode)
 
 setTimeout(() => {
